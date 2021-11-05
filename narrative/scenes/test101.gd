b5b4d115ce4ffee55aec1a5505952adea0b7c721
Tom Smallridge <tom@smallridge.com.au>
2021-11-06 01:44:30 +0800

    more commenting

--- narrative/scenes/test101.gd
+++ narrative/scenes/test101.gd
@@ -4,7 +4,9 @@ func _ready():
 # Narrative sequences that react to something in the world - e.g. a button is pressed
 # These are triggered by signals connected to the 'SceneNarrativeHandler's 'display_callback' function.
 	scene_callbacks = {
+		# The name of the callback event
 		"lamp_button_prompt": [
+			# The sequence of events that happen in reaction to the event
 			SceneDialogue.new("peter","[color=white][i]I could use this...[/i][/color]"),
 			Wait.new(0.5),
 			SceneDialogue.new("peter","[color=white]welp...[/color]"),
