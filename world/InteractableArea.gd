dec4290aaac950ad4fbff1d5b12b41500bb083d0
Tom Smallridge <tom@smallridge.com.au>
2021-11-05 11:57:16 +0800

    disabling and enabling detection zones and buttons

--- world/InteractableArea.gd
+++ world/InteractableArea.gd
@@ -6,10 +6,18 @@ signal triggered
 export(bool) var one_shot := false
 var has_been_triggered := false
 
+export(bool) var is_disabled := false
+
 func trigger():
 	if can_be_used():
 		emit_signal("triggered")
 		has_been_triggered = true
 
 func can_be_used() -> bool:
-	return not one_shot or (one_shot and not has_been_triggered)
+	return (not is_disabled) and (not one_shot or (one_shot and not has_been_triggered))
+
+func disable():
+	is_disabled = true
+
+func enable():
+	is_disabled = false
--- world/PlayerDetectionArea.gd
+++ world/PlayerDetectionArea.gd
@@ -8,17 +8,28 @@ var has_been_triggered := false
 
 var has_been_exited := false
 
+export(bool) var is_disabled := false 
+
 func on_player_entered(body: Node) -> void:
+	if is_disabled:
+		return
 	if not body.is_in_group("Player"):
 		return
 	if not one_shot or (one_shot and has_been_triggered):
 		has_been_triggered = true
 		emit_signal("player_entered")
 
-
 func on_player_exited(body: Node) -> void:
+	if is_disabled:
+		return
 	if not body.is_in_group("Player"):
 		return
 	if not one_shot or (one_shot and has_been_exited):
 		has_been_exited = true
 		emit_signal("player_exited")
+
+func disable():
+	is_disabled = true
+
+func enable():
+	is_disabled = false
